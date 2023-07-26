d5a1cecd44620796e3f53c3d714d6107150bb666
unknown <yurimarcelo2018@gmail.com>
2023-07-25 23:26:57 -0300

    Atualizações finais

--- Script/app.js
+++ Script/app.js
@@ -1,6 +1,12 @@
 import { valida } from "../Script/validacao.js";
 
+const form = document.querySelector('.formulario');
 const inputs = document.querySelectorAll('input');
+console.log(inputs[0].validity)
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault()
+})
 
 inputs.forEach(input =>{
     input.addEventListener('blur',(evento) =>{
